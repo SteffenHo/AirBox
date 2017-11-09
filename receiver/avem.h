ed8679c1be67c27eff94b20ce8ec38e19bfce66a
Steffen H <stef.holt@yahoo.de>
2017-11-09 14:24:01 +0100

    Add avem

---
+++ receiver/avem.h
@@ -0,0 +1,53 @@
+#include "avemConfig.h"
+
+class Avem{
+  private:
+    int id;
+    char* name;
+    AvemConfig config;
+    int device_id;
+
+  public:
+    Avem(int p_id, const char* p_name, AvemConfig p_config, int p_device_id){
+      id=p_id;
+      device_id = p_device_id;
+      int length = 0;
+      char* add = p_name;
+      for(; add[length]; length++);
+      
+      this->name = (char*)malloc((length+1)*sizeof(char));
+      int i = 0;
+      for(; i< length+1; i++){
+        this->name[i] = p_name[i];
+      }
+      //this->name[i++] = 0;
+      this->config = p_config;
+    }
+
+    void print(){
+      Serial.print("Id: ");
+      Serial.print(id);
+      Serial.print("\nName: ");
+      Serial.print(this->name);
+      Serial.print("\nConfig: ");
+      this->config.print();
+      Serial.print("\nDeviceId: ");
+      Serial.print(device_id);
+    }
+
+    String toJson(){
+      const size_t bufferSize = JSON_OBJECT_SIZE(8);
+      DynamicJsonBuffer jsonBuffer(bufferSize);
+
+      JsonObject& root = jsonBuffer.createObject();
+      root["id"]= id;
+      root["name"] = name;
+      root["deviceId"] = device_id;
+      JsonObject& t = config.toJson(root);
+      t.printTo(Serial);
+      String jsonStr;
+      root.printTo(jsonStr);
+      return jsonStr;
+    }
+};
+
---
+++ receiver/avemConfig.h
@@ -0,0 +1,110 @@
+#include <ArduinoJson.h>
+
+class AvemConfig {
+  private:
+    unsigned long decimal;
+    unsigned int bit_length;
+    unsigned int pulse_length;
+    unsigned int protocol;
+
+  public:
+    AvemConfig(unsigned long p_decimal, unsigned int p_bit_length, unsigned int p_pulse_length, unsigned int p_protocol){
+      decimal=p_decimal;
+      bit_length=p_bit_length;
+      pulse_length=p_pulse_length;
+      protocol=p_protocol;
+    }
+
+    AvemConfig(){
+      decimal=0;
+      bit_length=0;
+      pulse_length=0;
+      protocol=0;
+    }
+
+    AemConfig(const AvemConfig &obj){
+      decimal = obj.decimal;
+      bit_length = obj.bit_length;
+      pulse_length = obj.pulse_length;
+      protocol = obj.protocol;
+    }
+
+    char* bin2tristate(const char* bin) {
+      static char returnValue[50];
+      int pos = 0;
+      int pos2 = 0;
+      while (bin[pos]!='\0' && bin[pos+1]!='\0') {
+        if (bin[pos]=='0' && bin[pos+1]=='0') {
+          returnValue[pos2] = '0';
+        } else if (bin[pos]=='1' && bin[pos+1]=='1') {
+          returnValue[pos2] = '1';
+        } else if (bin[pos]=='0' && bin[pos+1]=='1') {
+          returnValue[pos2] = 'F';
+        } else {
+          return "not applicable";
+        }
+        pos = pos+2;
+        pos2++;
+      }
+      returnValue[pos2] = '\0';
+      return returnValue;
+    }
+
+    char* dec2binWzerofill(unsigned long Dec, unsigned int bitLength) {
+        static char bin[64]; 
+        unsigned int i=0;
+      
+        while (Dec > 0) {
+          bin[32+i++] = ((Dec & 1) > 0) ? '1' : '0';
+          Dec = Dec >> 1;
+        }
+      
+        for (unsigned int j = 0; j< bitLength; j++) {
+          if (j >= bitLength - i) {
+            bin[j] = bin[ 31 + i - (j - (bitLength - i)) ];
+          } else {
+            bin[j] = '0';
+          }
+        }
+        bin[bitLength] = '\0';
+        
+        return bin;
+      }
+
+    void print(){
+      if (decimal == 0) {
+        Serial.print("Unknown encoding.");
+        Serial.print(decimal);
+        Serial.print( bit_length );
+        Serial.print(pulse_length);
+        Serial.println(protocol);
+      } else {
+        const char* b = dec2binWzerofill(decimal, bit_length);
+        Serial.print(" \nDecimal: ");
+        Serial.print(decimal);
+        Serial.print(" (");
+        Serial.print( bit_length );
+        Serial.print("Bit) Binary: ");
+        Serial.print( b );
+        Serial.print(" \nTri-State: ");
+        Serial.print( bin2tristate( b) );
+        Serial.print(" \nPulseLength: ");
+        Serial.print(pulse_length);
+        Serial.print(" microseconds");
+        Serial.print(" \nProtocol: ");
+        Serial.println(protocol);
+      }
+    }
+
+   JsonObject& toJson(JsonObject &root){
+    JsonObject& config = root.createNestedObject("config");
+    config["decimal"] = decimal;
+    config["bitLength"] = bit_length;
+    config["pulseLength"] = pulse_length;
+    config["protocol"] = protocol;
+    
+    //root.printTo(Serial);
+    return root;
+   }
+};
+
