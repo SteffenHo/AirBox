5c0c0bf9825c9c566e32501569d6ce5f140ef5ba
Michael Braun <michael@web4brauns.de>
2017-11-16 14:54:20 +0100

    Add body to response

--- server/HttpResponse.h
+++ server/HttpResponse.h
@@ -3,10 +3,13 @@
 
 #include "HttpHeader.h"
 
+#define MAX_RESPONSE_BODY_SIZE 2048
+
 class HttpResponse {
   public:
     unsigned int statusCode = 500;
     HttpHeader header;
+    char body[MAX_RESPONSE_BODY_SIZE];
     char ready = 0;
     void send(WiFiClient &client, HttpResponse &p_response);
     void clear();
--- server/HttpResponse.ino
+++ server/HttpResponse.ino
@@ -22,12 +22,16 @@ Serial.println("client send");
   // start body
   client.println();
 
+  client.println(response.body);
+
+/*
   client.println("<!DOCTYPE HTML><html><head>");
   client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
   client.println("<h1>ESP32 - Web Server</h1>");
   client.println("<p>LED #1 <a href=\"on1\"><button>ON</button></a>&nbsp;<a href=\"off1\"><button>OFF</button></a></p>");
   client.println("<p>LED #2 <a href=\"on2\"><button>ON</button></a>&nbsp;<a href=\"off2\"><button>OFF</button></a></p>");
   client.println("</html>");
+  */
 }
 
 void HttpResponse::clear() {
