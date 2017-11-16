559b0d8a5c9c1b34f041b156dfb9ba21807dd46a
Michael Braun <michael@web4brauns.de>
2017-11-16 11:34:37 +0100

    Add string_copy_header to string_utils

--- server/string_utils.ino
+++ server/string_utils.ino
@@ -12,3 +12,19 @@ int string_utils_count_char(const char* charArray, char search) {
   return i;
 }
 
+void string_copy_header(char* new_string, const char* string) {
+  int length = strlen(string);
+  
+  for(int i = 0, j = 0; i <= length; i++) {
+    if(string[i] == '\n') {
+      continue;
+    }
+
+    if(j == 0 && string[i] == ' ') {
+      continue;
+    }
+
+    new_string[j++] = string[i];
+  }
+}
+
