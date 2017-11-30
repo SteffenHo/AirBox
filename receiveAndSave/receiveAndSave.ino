423947cae465570a07a1137914cd32ca1c2e805a
Steffen H <stef.holt@yahoo.de>
2017-11-30 14:52:48 +0100

    Add function to controll and test

--- receiveAndSave/receiveAndSave.ino
+++ receiveAndSave/receiveAndSave.ino
@@ -2,27 +2,34 @@
 #include "avem.h"
 #include <RCSwitch.h>
 #include "helper.h"
+#include "SDCardHelper.h"
 
 RCSwitch receiver = RCSwitch();
-
+//RCSwitch sender = RCSwitch();
+bool receiverIsActive = false;
+bool sendIsActive =  false;
+bool isProcessingTask = false;
 void setup() {
   // put your setup code here, to run once:
 
  Serial.begin(9600);
+ delay(20);
  receiver.enableReceive(D4);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+
+  initSD();
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Serial.println("z");
+  serialEvent();
+  
+  
   if (receiver.available()) {
-    
-    //output(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
-    AvemConfig av(receiver.getReceivedValue(), receiver.getReceivedBitlength(), receiver.getReceivedDelay(),receiver.getReceivedProtocol());
-    Avem a(2, "test", av, 2);
-    createAvemString(a);
     if(receiverIsActive && !isProcessingTask){
       isProcessingTask = true;
       //output(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
