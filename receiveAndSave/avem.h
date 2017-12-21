c3ff46f5d9ba4716f8e3cdaf5e418c6e7afdd5cc
Michael Braun <michael@web4brauns.de>
2017-12-21 10:04:15 +0100

    Remove unused code/comments Add DEV-flag Fix serialization in toJson

--- receiveAndSave/avem.h
+++ receiveAndSave/avem.h
@@ -1,72 +1,47 @@
 #ifndef _AVEM_H
 #define _AVEM_H
+
+#define AVEM_LENGTH 256
+
 #include "avemConfig.h"
-#define JSON_ARRAY_SIZE 200
 
 class Avem{
   public:
     AvemConfig config;
   private:
     int id;
-    char name[20];
+    char name[AVEM_LENGTH];
     int device_id;
 
   public:
-    Avem(int p_id, const char* p_name,const AvemConfig &p_config, int p_device_id){
+    Avem(int p_id, const char* p_name, int p_device_id){
       id=p_id;
       device_id = p_device_id;
-      int length = 0;
-      const char* add = p_name;
-      for(; add[length]; length++);
+      int length = strlen(p_name);
       
-      //this->name = (char*)malloc((length+1)*sizeof(char));
       int i = 0;
       for(; i< length || i<19; i++){
         name[i] = p_name[i];
       }
       this->name[i++] = 0;
+    }
+  
+    Avem(int p_id, const char* p_name, const AvemConfig &p_config, int p_device_id) : Avem(p_id, p_name, p_device_id) {
       config = p_config;
     }
-    Avem(int p_id, const char* p_name,int p_device_id, unsigned long p_decimal, unsigned int p_bit_length, unsigned int p_pulse_length, unsigned int p_protocol){
-      id=p_id;
-      device_id = p_device_id;
-      int length = 0;
-      const char* add = p_name;
-      for(; add[length]; length++);
-      
-      //this->name = (char*)malloc((length+1)*sizeof(char));
-      int i = 0;
-      for(; i< length || i<19; i++){
-        name[i] = p_name[i];
-      }
-      this->name[i++] = 0;
-      config = AvemConfig(p_decimal, p_bit_length, p_pulse_length, p_protocol);
-        
-    }
-
-   /*Avem(const Avem &av){
-    Serial.println("Copy constructor allocating  Avem");
-    id = av.id;
-    device_id = av.device_id;
-    int length = 0;
-      char* add = av.name;
-      for(; add[length]; length++);
-      
-      this->name = (char*)malloc((length+1)*sizeof(char));
-      int i = 0;
-      for(; i< length; i++){
-        this->name[i] = av.name[i];
-      }
-      this->name[i++] = 0;
-      config = av.config;
     
-   }*/
+    Avem(int p_id, const char* p_name,int p_device_id, unsigned long p_decimal, unsigned int p_bit_length, unsigned int p_pulse_length, unsigned int p_protocol)
+    : Avem(p_id, p_name, p_device_id) {
+      config = AvemConfig(p_decimal, p_bit_length, p_pulse_length, p_protocol);
+    }
+    
     int getId() const {return id;}
     int getProtocol() const { return config.protocol;};
     int getPulseLength() const { return config.pulse_length;};
     char* getTriState() const { return config.getTriState();};
 
     void print() const{
+#ifdef DEV
       Serial.print("Id: ");
       Serial.print(id);
       Serial.print("\nName: ");
@@ -75,25 +50,21 @@ class Avem{
       this->config.print();
       Serial.print("\nDeviceId: ");
       Serial.print(device_id);
+#endif
     }
 
-    char* toJson() const{
-      const size_t bufferSize = JSON_OBJECT_SIZE(8);
-      DynamicJsonBuffer jsonBuffer(bufferSize);
+    void toJson(char* buffer, int pLength) const{
+      StaticJsonBuffer<200> jsonBuffer;
 
       JsonObject& root = jsonBuffer.createObject();
       root["id"]= id;
       root["name"] = name;
       root["deviceId"] = device_id;
       JsonObject& t = config.toJson(root);
-   
-      char jsonChar[JSON_ARRAY_SIZE];
-      t.printTo(jsonChar, sizeof(char)*JSON_ARRAY_SIZE);
-    
-      return jsonChar;
+      
+      int length = t.printTo(buffer, pLength);
     }
 
 };
+
 #endif
-
-
