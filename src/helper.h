ebac0727644dca1153f3b843062adb35b07c7879
Steffen H <stef.holt@yahoo.de>
2018-01-11 14:09:30 +0100

    Add function to create  device string

--- src/helper.h
+++ src/helper.h
@@ -35,7 +35,36 @@ void createAvemString(const Avem &av ){
 }
 
 
+void createDeviceString(const Device &dv ){
+  Serial.println("createDeviceString");
+  char data[AVEM_SERIALIZE_BUFFER_SIZE];
+   char idStr[10];
+   int i = 0;
 
+  char str[AVEM_SERIALIZE_BUFFER_SIZE];
+  dv.toJson(str, AVEM_SERIALIZE_BUFFER_SIZE);
+  
+  int jsonStrLen = strlen(str);
+  itoa(dv.getId(), idStr, 10);
+  int idStrLen = strlen(idStr);
+ 
+  for(int j = 0;i<idStrLen; i++, j++){
+    data[i] = idStr[j];
+  }
+  
+  data[i++] = SEPARATOR;
+  
+  for(int k = 0; i<idStrLen+jsonStrLen+2; i++, k++){
+    data[i] = str[k];
+  }
+  data[i++] = 0;
+
+#ifdef __DEV__
+  Serial.println(data);
+#endif
+  
+  addStringToFile(data, DEVICE_DB_FILE_NAME);
+  }
 
 
 #endif
