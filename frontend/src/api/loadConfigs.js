f8eef7e265bfc8afc888357f93cd926e5ae682ae
Michael Braun <michael@web4brauns.de>
2018-01-18 09:07:12 +0100

    Remove sample data

--- frontend/src/api/loadConfigs.js
+++ frontend/src/api/loadConfigs.js
@@ -1,29 +1,4 @@
 export default function loadConfigs() {
-    return Promise.resolve([
-        {
-            "id": 15,
-            "name": "test-on",
-            "deviceId": 1,
-            "config": {
-                "decimal": 1381717,
-                "bitLength": 24,
-                "pulseLength": 424,
-                "protocol": 1
-            }
-        },
-        {
-            "id": 16,
-            "name": "test-off",
-            "deviceId": 1,
-            "config": {
-                "decimal": 1381716,
-                "bitLength": 24,
-                "pulseLength": 424,
-                "protocol": 1
-            }
-        }
-    ]);
-
     return fetch('http://airbox/devices/configs', {
         headers: {
             'Cache-Control': 'no-cache',
