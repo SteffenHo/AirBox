a99b52291045bee2d74aa2a1d64ee95cbcd469f2
Michael Braun <michael@web4brauns.de>
2018-01-25 08:39:57 +0100

    Catch rejected promise

--- frontend/src/api/createConfig.js
+++ frontend/src/api/createConfig.js
@@ -3,5 +3,5 @@ import request from '../utils/request.js';
 const createConfig = (deviceId, name) => request('http://airbox/devices/configs', 'POST', null, {
     deviceId,
     name,
-});
+}).catch(() => Promise.resolve());
 export default createConfig;
